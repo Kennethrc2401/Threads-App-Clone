dfbd388bf0afff594a1295d4b0992a9ac480a113
Kenneth Castro <kennethramirezcastro@gmail.com>
2023-08-21 09:27:27 -0400

    First Commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // variable to check if the connection is established or not
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if(isConnected) return console.log('Already connected to MongoDB');
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+}
